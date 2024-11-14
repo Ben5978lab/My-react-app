721d69ec76b71b4ff89c90f2c8671a7c81ddfcf0
ben5978lab <ben.rose5978@gmail.com>
2024-11-14 11:24:56 +0100

    React basics 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const selectedPokemon = pokemonList[0]
+
+
 function App() {
   return (
     <section>
-      <PokemonCard />
+      <PokemonCard pokemon={selectedPokemon}/>
     </section>
   );
 }
